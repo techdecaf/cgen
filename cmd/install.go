ed8e3320f1d07c0e96b741cce9e5a30c5f7094bc
jon.ward <jon@techdecaf.com>
2019-09-04 12:08:13 -0700

    add install cmd

--- cmd/install.go
+++ cmd/install.go
@@ -1,23 +1,28 @@
 package cmd
 
 import (
-	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/techdecaf/cgen/app"
 )
 
 // installCmd represents the install command
 var installCmd = &cobra.Command{
 	Use:   "install",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Installs a new generator from a git repository",
+	Long:  `Installs a new generator from a git repository`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("install called")
+		// initialize a new instance of cgen
+		cgen := &app.CGen{}
+		if err := cgen.Init(); err != nil {
+			app.Log.Fatal("cgen_init", err)
+		}
+
+		if err := cgen.Install(args[0]); err != nil {
+			app.Log.Fatal("cgen_install", err)
+		}
+		os.Exit(0)
 	},
 }
 
