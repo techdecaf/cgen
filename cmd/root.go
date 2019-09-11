ab8b519461836122e78d37b662ce431a1f4fab72
Jon Ward <jon@techdecaf.com>
2019-09-11 06:54:28 -0700

    updates helptext

--- cmd/root.go
+++ cmd/root.go
@@ -23,13 +23,10 @@ var pwd, _ = os.Getwd()
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "cgen",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A cross platform plugin-based project generator",
+	Long: `You can use cgen to dynamically configure new projects based
+   on your own standards and best pratices. See the README.md to get started.
+  `,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
