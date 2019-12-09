eb4753f69df7cd921a177bc4ed0bd0ca7152bbb8
Jon Ward <jon@techdecaf.com>
2019-12-09 12:05:20 -0800

    fix: instal.ps1 failing on install path

--- install.ps1
+++ install.ps1
@@ -1,11 +1,15 @@
 $APP_NAME = "cgen"
 $BUCKET_URL = "http://github.techdecaf.io"
-$INSTALL_PATH = "$env:HOME/bin"
+# $INSTALL_PATH = "$env:USERPROFILE\bin"
+$INSTALL_PATH = "c:\windows\system32"
 
 $APP_BINARY = "$INSTALL_PATH\cgen.exe"
 $LATEST_STABLE = "$BUCKET_URL/$APP_NAME/latest/windows/$APP_NAME.exe"
 
-echo "[Installed] $APP_NAME version: $(&$APP_NAME --version)"
+if (Test-Path C:\Windows\System32\cgen.exe){
+  echo "[Installed] $APP_NAME version: $(&$APP_NAME --version)"
+}
+
 echo "[Installing] $LATEST_STABLE"
 
 Invoke-WebRequest -Uri "$LATEST_STABLE" -OutFile "$APP_BINARY"
